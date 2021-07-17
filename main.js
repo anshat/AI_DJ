05464d0e6a1592ef45b6b8f2e072a1bc8b6779ac
@nsh@ <46368312+anshat@users.noreply.github.com>
2021-07-17 15:46:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,86 @@
+song = "";
+
+function preload() {
+    song = loadSound("DangerWoman Ariana Grande.mp3");
+}
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modleLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+    fill("#11fa21");
+    stroke("#f50202");
+    if (scoreRightWrist > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        if (rightWristY > 0 && rightWristY <= 100) {
+            document.getElementById("speed").innerHTML = "speed is = 0.5x";
+            song.rate(0.5);
+
+        }
+        else if(rightWristY > 100 && rightWristY <=200){
+            document.getElementById("speed").innerHTML = "speed is = 1x";
+            song.rate(1);
+        }
+        else if(rightWristY > 200 && rightWristY <=300){
+            document.getElementById("speed").innerHTML = "speed is = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristY > 300 && rightWristY <=400){
+            document.getElementById("speed").innerHTML = "speed is = 2x";
+            song.rate(2);
+        }
+        else{
+            document.getElementById("speed").innerHTML = "speed is = 2.5x";
+            song.rate(2.5);
+        }
+    }
+    if(scoreLeftWrist>0.2){
+circle(leftWristX,leftWristY,20);
+X=Number(leftWristY);
+remove_decimal=floor(X);
+volume=remove_decimal/500;
+document.getElementById("volume").innerHTML="volume= "+volume;
+song.setVolume(volume);
+    }
+}
+
+function modleLoaded() {
+    console.log("posenet is initialized");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist x =" + leftWristX + "left wrist y=" + leftWristY);
+        rightWristX = results[0].pose.righttWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right wrist x =" + rightWristX + "right wrist y=" + rightWristY);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+    }
+}
+
+function play() {
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
+function pause() {
+    song.pause();
+}
